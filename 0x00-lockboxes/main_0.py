9583586bc257987cc84294483901e9676581d689
= <=>
2021-07-23 00:03:10 +0000

    x

--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-"""
-You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1
-and each box may contain keys to the other boxes.
-"""
 
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-def canUnlockAll(boxes):
-    if not boxes:
-        return False
-    if len(boxes) == 0:
-        return False
-    keys = []
-    keys.append(0)
-    for key in keys:
-        new_keys = boxes[key]
-        for new_key in new_keys:
-            if new_key not in keys and new_key < len(boxes):
-                keys.append(new_key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
