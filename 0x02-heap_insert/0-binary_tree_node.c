8f32910fd9a89ee8a2342f3c8a26689ed4e6b5a9
= <=>
2021-08-05 17:16:09 +0000

    x

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,21 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - insert into binary tree
- * @parent: root node
- * @value: value of the binary tree
- * Return: new node
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent node
+ * @value: integer for the new node
+ *
+ * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+    binary_tree_t *new;
 
-    new_node = malloc(sizeof(*new_node));
-    if (!new_node)
+    new = malloc(sizeof(binary_tree_t));
+    if (new == NULL)
         return (NULL);
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->n = value;
-    new_node->parent = parent;
-    return (new_node);
+    new->n = value;
+    new->parent = parent;
+    new->left = NULL;
+    new->right = NULL;
+
+    return (new);
 }
