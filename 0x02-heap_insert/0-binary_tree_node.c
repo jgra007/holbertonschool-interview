d19299a21fc741f1516b5bbd43a99c502d51fbe5
= <=>
2021-08-05 17:12:22 +0000

    x

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,22 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: parent node
- * @value: node value
- * Return: pointer to new node
+ *binary_tree_node - Making the initial node
+ *@parent: The parent node
+ *@value: The value being stored in the node
+ *Return: The returning the node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+    /*Structure pointer*/
+    binary_tree_t *New_node;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (!new)
+    /*Allocate memory*/
+    New_node = malloc(sizeof(binary_tree_t));
+
+    /*Validate if it is null*/
+    if (New_node == NULL)
         return (NULL);
-    new->n = value;
-    new->parent = parent;
-    new->left = NULL;
-    new->right = NULL;
-    return (new);
+
+    /*Add node*/
+    New_node->left = New_node->right = NULL;
+    New_node->n = value;
+    New_node->parent = parent;
+
+    return (New_node);
 }
