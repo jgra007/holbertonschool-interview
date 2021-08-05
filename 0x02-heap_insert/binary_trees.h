19c1daa6ec9301ec14c24fd2b52c3b51fde2fd3f
= <=>
2021-08-05 17:06:10 +0000

    uploading files

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,7 +1,6 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
-#include <stddef.h>
+#ifndef BINARY
+#define BINARY
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -19,10 +18,10 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
-typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s binary_tree_t;
-void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-heap_t *heap_insert(heap_t **root, int value);
+typedef struct binary_tree_s heap_t;
 
-#endif /* _BINARY_TREES_H_ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+#endif
