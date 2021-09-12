cb83b070818d6452c89331e1150e1a3c830dff2b
jgra007 <66275651+jgra007@users.noreply.github.com>
2021-09-12 16:34:44 -0500

    correctiion

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,34 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - check the linked list if is a cycle or not.
- * @list: is the linked list.
- * Return: Always 0.
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to head of list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *ptr1, *ptr2;
+    listint_t *slow = list;
+    listint_t *fast = list;
 
-	if (!list)
-		return (0);
+    if (list == NULL)
+        return (0);
 
-	ptr1 = list;
-	ptr2 = list->next;
+    while (slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
 
-	while (ptr1 != ptr2)
-	{
-		ptr1 = ptr1->next;
-		if (!ptr1)
-			return (0);
-		
-        ptr2 = ptr2->next;
-		if (!ptr2)
-			return (0);
-		
-        ptr2 = ptr2->next;
-		if (!ptr2)
-			return (0);
-	}
-	return (1);
+        if (slow == fast)
+            return (1);
+    }
+
+    return (0);
 }
