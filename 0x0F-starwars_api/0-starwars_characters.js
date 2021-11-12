fbd6080fe5ca912944412e544509725966e2576a
jgra007 <jgra007@hotmail.com>
2021-11-12 08:19:01 -0500

    fixed comments

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,30 +3,27 @@
 const request = require('request');
 const ac = process.argv.slice(2);
 
-request.get(`https://swapi-api.hbtn.io/api/films/${ac}/`, async (_error, response, body) => {
-  const characters = JSON.parse(body).characters;
-  for (const character of characters) {
-    const charac = await new Promise((resolve, reject) => {
-      request(character, (errors, responses, bodys) => {
-        if (errors) {
-          reject(errors);
-        } else {
-          resolve(JSON.parse(bodys).name);
-        }
-      });
-    });
-    console.log(charac);
-  }
-});
-<<<<<<< HEAD
-
-function recursionRequestPrint (url, index) {
-  request(url[index], (err, res, body) => {
-    if (!err) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+request.get(`https://swapi-api.hbtn.io/api/films/${ac}/`, async(_error, response, body) => {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+        const charac = await new Promise((resolve, reject) => {
+            request(character, (errors, responses, bodys) => {
+                if (errors) {
+                    reject(errors);
+                } else {
+                    resolve(JSON.parse(bodys).name);
+                }
+            });
+        });
+        console.log(charac);
     }
-  });
-} 
-=======
->>>>>>> ab6859ac01041f1f8ef07987ad4eadd9ce04896b
+});
+
+function recursionRequestPrint(url, index) {
+    request(url[index], (err, res, body) => {
+        if (!err) {
+            console.log(JSON.parse(body).name);
+            if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+        }
+    });
+}
